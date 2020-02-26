d86a4ebac2421dda0e16e4cb1e4c187b7d6de84f
Oscar Diaz <odiaz.dw@gmail.com>
2020-02-26 00:07:53 -0600

    adding model viewer

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 var stateInterval = null;
 
 function init() {
-  initAR();
+  
 }
 
-function initAR() {
+function fallbackAR() {
   // Check whether this is an Android device.
   const isAndroid = /android/i.test(navigator.userAgent);
   // This web fallback url will launch if the correct version of the Google app is
   // not available.
   const fallbackUrl =
 
-  'https://arvr.google.com/scene-viewer?file=https%3A%2F%2Fstorage.googleapis.com%2Far-answers-in-search-models%2Fstatic%2FTiger%2Fmodel.glb&link=https%3A%2F%2Fgoogle.com&title=Tiger';
+  'raw.githubusercontent.com/odiazjs/arcore-colombia/master/assets/goldenboot/Botin%20de%20Oro.gltf&link=https%3A%2F%2Fgoogle.com&title=Tiger';
 
   // This is the intent launching URL that triggers Scene Viewer on Android
   // and falls back to fallbackUrl if the Google app is not available.
   const sceneViewerUrl =
 
-  'intent://storage.googleapis.com/ar-answers-in-search-models/static/Tiger/model.glb?title=Tiger#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=' +
+  'intent://raw.githubusercontent.com/odiazjs/arcore-colombia/master/assets/goldenboot/Botin%20de%20Oro.gltf?title=Goldenboot#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=' +
       fallbackUrl + ';end;';
 
   // Create a link.
